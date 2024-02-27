b93634ec9bdacad7b4216e4a90fcca2c642337c9
kalpana <tanwarkalpna@gmail.com>
2024-02-27 15:28:49 +0100

    commit
    stuff

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Transformations = () => {
+  return (
+    <div>
+      Transformations
+    </div>
+  )
+}
+
+export default Transformations
+
+
+//localhost:3000/transformations/[id]- for this we use dynamic routes in Nextjs 14
